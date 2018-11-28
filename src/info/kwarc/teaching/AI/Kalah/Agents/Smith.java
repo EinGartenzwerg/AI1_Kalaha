004323a3679e352be6888c9be27a0ffc21d9c636
ESE-Move <ESE-Move@users.noreply.github.com>
2018-11-28 01:33:04 +0100

    init code

---
+++ src/info/kwarc/teaching/AI/Kalah/Agents/Smith.java
@@ -0,0 +1,33 @@
+package info.kwarc.teaching.AI.Kalah.Agents;
+
+import info.kwarc.teaching.AI.Kalah.Board;
+
+public class Smith extends info.kwarc.teaching.AI.Kalah.Agents.Agent {
+
+	@Override
+	public String name() {
+		return "Smith";
+	}
+
+
+	@Override
+	public void init(Board board, boolean playerOne) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int move() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public Iterable<String> students() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
